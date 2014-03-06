513b259be59170bce544454fce61c808b19b4138
kenyu-uema <kenyu.uema@tafs.co.jp>
2014-03-06 15:00:46 +0900

    sql creating

--- src/main/java/websystem/service/GameSearchService.java
+++ src/main/java/websystem/service/GameSearchService.java
@@ -3,15 +3,27 @@ package websystem.service;
 import java.util.List;
 
 import org.seasar.extension.jdbc.JdbcManager;
+import org.seasar.framework.container.SingletonS2Container;
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
 
 import websystem.dto.GameBean;
 
 public class GameSearchService {
 
 	private static final String SQL_FILE = "META-INF/sql/websystem/entity/Employee/serach.sql";
-	public JdbcManager jdbcManager;
 
-	public String getSqlOfSelectGameList(JdbcManager jdbcManager) {
+	public static void main(String[] args) {
+		SingletonS2ContainerFactory.init();
+		try {
+			JdbcManager jdbcManager = SingletonS2Container
+					.getComponent(JdbcManager.class);
+			SqlOfSelectGameList(jdbcManager);
+		} finally {
+			SingletonS2ContainerFactory.destroy();
+		}
+	}
+
+	public static String SqlOfSelectGameList(JdbcManager jdbcManager) {
 		List<GameBean> sql = jdbcManager
 				.selectBySqlFile(GameBean.class, SQL_FILE)
 				.getResultList();
